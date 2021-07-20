dea082cc65181324a67c8c9bfd15a885f04a5560
Shauna Mack <shaunakatemack@gmail.com>
2021-07-20 23:54:05 +1000

    use list for dynamic data

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # home with selected posts
     path("", views.index, name="starting-page"),
     # all blog posts
-    path("posts", views.all_posts, name="posts-page"),
+    path("posts", views.posts, name="posts-page"),
     # individual blog post page
     # slug is unique-identifier that is SEO and user friendly
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,77 @@
 from django.shortcuts import render
-import datetime
+from datetime import date
 from operator import itemgetter
 
-blog_posts = {
-    "post_one": {
-        'content': "I am post 1",
-        'created_at': datetime.datetime(2021, 7, 12)
-    },
-    "post_two": {
-        'content': "I am post 2",
-        'created_at': datetime.datetime(2021, 7, 15)
-    },
-    "post_three": {
-        'content': "I am post 3",
-        'created_at': datetime.datetime(2021, 7, 22)
-    },
-}
+all_posts = [
+    {
+        'slug': "richard-good-one",
+        'image': "catdesk.JPG",
+        'date': date(2021, 7, 12),
+        'title': "good one",
+        'excerpt': "meow meow meow woof meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow ",
+        'content': "meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow"
 
-# Create your views here.
+    },
+    {
+        'slug': "richard-good-two",
+        'image': "dundee.JPG",
+        'date': date(2021, 7, 13),
+        'title': "vg one",
+        'excerpt': "lick meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow ",
+        'content': """
+        meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow 
+        """
+
+    },
+    {
+        'slug': "richard-good-three",
+        'image': "Rick.JPG",
+        'date': date(2021, 7, 21),
+        'title': "b one",
+        'excerpt': "purrkfsfd meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow ",
+        'content': """
+        meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow eow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow 
+        """
+
+    }
+]
+
+# Create your views here.\
+
+# date helper
+
+
+def get_date(post):
+    return post['date']
+
 
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
     # for post in blog_posts.keys():
     #     blog_posts['post']['created_at']
 
     # dict(sorted(blog_posts.items(), key = itemgetter(1))[2:])
-    # return 
+    # return
     pass
 
 
-def all_posts(request):
+def posts(request):
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
     # return render(request, 'blog/posts.html', {
     #     'blog_posts': blog_posts
     # })
     pass
 
-def post_detail(request):
+
+def post_detail(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
     pass
